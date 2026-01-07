c544498e43b99eb8a93245a1b67149fd4cb09cbb
IFearF4ded <tiloualex88@gmail.com>
2026-01-07 15:42:27 -0700

    Replace AutoRTPER with TPASpammer module

--- src/main/java/com/nnpg/glazed/GlazedAddon.java
+++ src/main/java/com/nnpg/glazed/GlazedAddon.java
@@ -113,7 +113,7 @@ public static final Category pvp = new Category("Glazed PVP", new ItemStack(Item
         Modules.get().add(new AutoTotemOrder());
         Modules.get().add(new LightESP());
         Modules.get().add(new FreecamV2());
-        Modules.get().add(new AutoRTPER());
+        Modules.get().add(new TPASpammer());
     }
 
     @EventHandler
